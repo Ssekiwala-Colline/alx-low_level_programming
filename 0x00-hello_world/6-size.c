0f3136d579a5cac4c822a06bcd472feb903475e5
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-08 17:32:48 +0000

    datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 /**
-  *main - print different sizes of different data types 
-  *Return: like float, int, double and char
-  */
+*main - print different sizes of different data types 
+*Return: like float, int, double and char
+*/
 int main(void)
 {
-	int int_type;
-	float float_type;
-	long long_type;
 	char char_type;
+	int int_type;
+	long long_type;
+	long long long_long;
+	float float_type;
 
-	printf("Size of int_type: %zu\n", sizeof(int_type));
-	printf("Size of float_type: %zu\n", sizeof(float_type));
 	printf("Size of char_type: %zu\n", sizeof(char_type));
+	printf("Size of int_type: %zu\n", sizeof(int_type));
 	printf("Size of long_type: %zu\n", sizeof(long_type));
+	printf("Size of long_long: %zu\n", sizeof(long_long));
+	printf("Size of float_type: %zu\n", sizeof(float_type));
 	return (0);
 }
