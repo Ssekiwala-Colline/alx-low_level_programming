c80383a1d49a8be18198ff94a89c3a7cfe55ca1c
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-08 15:06:00 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-   *Use puts function instead of printf
-   *The puts function prints an output followed by a new line
-   *Unless used with the syntax fputs(string, stdout)
-   *printf prints a long output with values of variables
-   */
+*Use puts function instead of printf
+*The puts function prints an output followed by a new line
+*Unless used with the syntax fputs(string, stdout)
+*printf prints a long output with values of variables
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
