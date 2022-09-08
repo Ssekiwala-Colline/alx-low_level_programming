7d4abc4cc8ce98b12371a3bb3a8979d85b8eb7f3
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-08 14:18:18 +0000

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/*
+/**
    *Use puts function instead of printf
    *The puts function prints an output followed by a new line
    *Unless used with the syntax fputs(string, stdout)
    *printf prints a long output with values of variables
-*/
+   */
 int main(void)
 {
-	/*This is the body which contains the code block of main*/
+	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
