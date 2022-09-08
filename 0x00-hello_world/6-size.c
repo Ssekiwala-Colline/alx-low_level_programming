b3849b0ee291cf03e9216d427e71166fd31f6ae2
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-08 20:39:40 +0000

    datatype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-*main - print different sizes of different data types 
+ *main - print different sizes of different data types 
 *Return: like float, int, double and char
 */
 int main(void)
