4313495d581f4064fdbc1b62d86e0829725eac7d
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-08 17:44:08 +0000

    datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long long_long;
 	float float_type;
 
-	printf("Size of char_type: %zu\n", sizeof(char_type));
-	printf("Size of int_type: %zu\n", sizeof(int_type));
-	printf("Size of long_type: %zu\n", sizeof(long_type));
-	printf("Size of long_long: %zu\n", sizeof(long_long));
-	printf("Size of float_type: %zu\n", sizeof(float_type));
+	printf("Size of a char: %zu\n", sizeof(char_type));
+	printf("Size of an int: %zu\n", sizeof(int_type));
+	printf("Size of a long int: %zu\n", sizeof(long_type));
+	printf("Size of a long long int: %zu\n", sizeof(long_long));
+	printf("Size of a float: %zu\n", sizeof(float_type));
 	return (0);
 }
