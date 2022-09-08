7f526395589bff46f96f1ca41cf00e0c488ba6f5
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-08 14:40:34 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  *Printf prints an output with values of variables
+  *like \n, \d
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
