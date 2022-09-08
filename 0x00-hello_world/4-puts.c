d428c41ea36c8eeec5429df29ccef251c02a4460
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-08 13:41:51 +0000

    Prints output followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+   *Use puts function instead of printf
+   *The puts function prints an output followed by a new line
+   *Unless used with the syntax fputs(string, stdout)
+   *printf prints a long output with values of variables
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
