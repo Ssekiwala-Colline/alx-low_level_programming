5722286a80207618de4dbc0c80c19d3c86bc2ff6
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-11 10:58:38 +0000

    lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - The program that prints lowercase
+*Return: alphabet followed by a new line
+*/
+int main(void)
+{
+	char ch;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar ('\n');
+	return (0);
+}
