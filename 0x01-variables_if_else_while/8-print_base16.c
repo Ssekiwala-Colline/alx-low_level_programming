ceeb0ebd6b6bd1032a3c05a089bbce5795861816
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-11 21:50:14 +0000

    print_

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - The program that prints 
+Return: the numbers of base 16 in lowercase
+*/
+int main(void)
+{
+	int k;
+	char j;
+
+	for(k = '0'; k <= '9'; k++)
+	{
+		putchar(k);
+	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
