5684eb15e33d0ccd566055c3c56d90f54054fa16
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-11 12:24:44 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - The program prints digit numbers
+*Return: from 0 to 9
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf(" %d", i);
+	}
+	printf("\n");
+	return (0);
+}
