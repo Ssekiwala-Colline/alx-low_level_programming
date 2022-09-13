2b9c4138e2068e5081027d8ecfc222f42f905cd2
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-13 02:49:29 +0000

    print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,5 +6,19 @@
 */
 int main(void)
 {
-	
+	int m;
+
+	for (m = 0; m <= 9; m++)
+	{
+		putchar((m % 10) + '0');
+		if (m == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
 }
