de8d27a24e591548976bb1cf3cdbadc813041006
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-16 20:42:17 +0000

    The program

---
+++ alx-low_level_programming/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*main - The program that uses a function
+*Return: To check for zero number
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
