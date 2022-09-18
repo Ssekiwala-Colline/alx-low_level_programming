a152c1262ccf949100f3d6297e6718558f5752c6
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-18 14:58:31 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - prints the alphabet, in lowercase, followed by a new line
+*Return: return 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i >= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	return (0);
+}
