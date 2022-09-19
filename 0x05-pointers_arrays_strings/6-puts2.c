a066c35982afd2ae34ae2ce58f370c14f79f398b
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-19 20:17:22 +0000

    haracter of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+"puts2 - prints every other character of a string, starting with the first character, followed by a new line.
+*@str: string
+*
+*Return: nothing
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\n')
+	{
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
+	}
+	putchar (10);
+}
