8443a9bbd1df869429159de99326597646c5711d
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-19 17:51:17 +0000

    Pointer to int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - Takes the pointer to an int as a parameter
+*
+*Return: Always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 402;
+	
+	_putchar(n);
+
+	void reset_to_98(int *n);
+
+	_putchar(n);
+
+	return (0);
+}
