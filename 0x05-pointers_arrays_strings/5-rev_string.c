db84a68bbaba8e31445ab249a1ca51174ecc6ef2
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-20 09:41:23 +0000

    reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,24 @@
-#
+#include "main.h"
+#include <string.h>
+
+/**
+*rev_string -  function that reverses a string
+*@s: string
+*Return: Reversed string
+*/
+
+void rev_string(char *s)
+{
+	int i, j;
+
+	char ch;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+		for (j = 0; j < i/2; j++)
+		{
+			ch = s[i];
+			s[i] = s[i - 1 - j];
+			s[i - 1 - j] = ch;
+		}
+}
