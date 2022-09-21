145675e4d072cfabe608b5657d1bfc65c15f7604
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-09-21 17:28:30 +0000

    lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*string_toupper changes all lowercase letters to uppercase
+*string_toupper - changes all lowercase letters to uppercase
 *@str: string to be changed
 *
 *Return: address to the string
