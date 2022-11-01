573591da8981e4eaf9801ebd8c22d92c03ecd02f
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-11-01 11:13:15 +0000

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,4 @@
-fndef __MAIN_H__
+#ifndef __MAIN_H__
 #define __MAIN_H__
 
 #include <sys/types.h>
@@ -11,4 +11,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /*__MAIN_H__*/
