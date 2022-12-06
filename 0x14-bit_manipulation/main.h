b75b4a5d832c68936b1d286372eb415e385d7c95
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-12-06 07:15:05 +0000

    Added 0-binary_to_uint.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
  */
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
